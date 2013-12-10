b93b13f7db2358d6fa023f5736e21b536cfc39d5
somrahul <somrahul@umich.edu>
2013-12-10 15:24:19 -0500

    logout code

--- logout.php
+++ logout.php
@@ -1,4 +1,5 @@
 <?php
     session_start();
     session_destroy();
-    header("Location: index.php");
+    //document.location.href = "https://www.google.com/accounts/Logout?continue=https://appengine.google.com/_ah/logout?continue=http://www.mysite.com";
+    header("Location: https://www.google.com/accounts/Logout?continue=https://appengine.google.com/_ah/logout?continue=http://localhost:8888/tradeCollab");
